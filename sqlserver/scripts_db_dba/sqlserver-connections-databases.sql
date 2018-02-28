c7b69b663374dad27b6667ed3049531979ccf472
Alex Rondão <alexrondao@hotmail.com>
2018-02-28 14:10:12 -0300

    adicionando mais conteudo

--- sqlserver/scripts_db_dba/sqlserver-connections-databases.sql
+++ sqlserver/scripts_db_dba/sqlserver-connections-databases.sql
@@ -16,3 +16,28 @@ WHERE
     dbid > 0
 GROUP BY 
     dbid, loginame;
+
+SELECT DB_NAME(sP.dbid) AS the_database
+	, COUNT(sP.spid) AS total_database_connections
+FROM sys.sysprocesses sP
+GROUP BY DB_NAME(sP.dbid)
+ORDER BY 1;
+
+--=====================================================
+--Database Connections Using dm_os_performance_counters
+--=====================================================
+SELECT oPC.cntr_value AS connection_count
+FROM sys.dm_os_performance_counters oPC
+WHERE 
+	(
+		oPC.[object_name] = 'SQLServer:General Statistics'
+			AND oPC.counter_name = 'User Connections'
+	)
+ORDER BY 1;
+
+SELECT * FROM sys.sysprocesses
+WHERE 
+    dbid > 0
+	and loginame = 'usrportal'
+	and DB_NAME(dbid) = 'DIGITAL'
+	and program_name = 'DigitalAPI'
